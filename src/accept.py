0b986d9bad0ad76464ff09107fc4045fc8456219
CJ Adams <yaakovred@gmail.com>
2017-03-30 20:18:14 -0400

    worked on mqtt, accessed server

--- src/accept.py
+++ src/accept.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import paho.mqtt
+import paho.mqtt.client
 
 __appname__     = ""
-__author__      = "Marco Sirabella"
+__author__      = "CJ Adams"
 __copyright__   = ""
 __credits__     = ["Marco Sirabella"]  # Authors and bug reporters
 __license__     = "GPL"
@@ -13,3 +13,16 @@ __maintainers__ = "Marco Sirabella"
 __email__       = "msirabel@gmail.com"
 __status__      = "Prototype"  # "Prototype", "Development" or "Production"
 __module__      = ""
+
+class MyClient(paho.mqtt.client.Client):
+    def __init__(self, server, creds):
+        super().__init__(client_id="db_server", clean_session=False)
+        self.username_pw_set(creds[0], creds[1])
+	self.connect(server[0], server[1])
+
+with open("mqtt.auth", "r") as file:
+    server = file.readline().split()
+    credentials = file.readline().split()
+mqttc = MyClient(server, credentials)
+mqttc.subscribe("plants/*/moisture")
+
